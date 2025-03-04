0b8efa5198d369a420a2303b735dc9540c1892b4
imariamabdullah <mariam99hendy@gmail.com>
2025-03-04 10:27:45 +0200

    create bases of project

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+    // UI
+    require_once './shared/header.php';
+    require_once './shared/navbar.php';
+?>
+
+
+<div class="continer">
+    <h1 class="display-1">hello from display</h1>
+    
+</div>
+
+<?php 
+require_once './shared/footer.php';
+
+?>
+
+   
---
+++ shared/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Company</title>
+    
+    <link rel="stylesheet" href="./assets/vendors/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/main.css">
+    
+</head>
+<body>
