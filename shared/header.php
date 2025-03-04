407240db1eef5775e2c99e1a3693d3a9e7697912
imariamabdullah <mariam99hendy@gmail.com>
2025-03-04 11:06:50 +0200

    link URL fun with header.php file

--- shared/header.php
+++ shared/header.php
@@ -1,3 +1,8 @@
+<?php 
+// config
+require_once "C:/xampp/htdocs/backend/session_9/company31/app/functions.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,8 +10,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Company</title>
     
-    <link rel="stylesheet" href="./assets/vendors/bootstrap/bootstrap.min.css">
-    <link rel="stylesheet" href="./assets/css/main.css">
+    <link rel="stylesheet" href="<?php URL('assets/vendors/bootstrap/bootstrap.min.css')?>">
+    <link rel="stylesheet" href="<?php URL('assets/css/main.css') ?>">
     
 </head>
 <body>
