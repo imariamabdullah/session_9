983f25abbae43fb2298394a157d145ca5d34ebba
imariamabdullah <mariam99hendy@gmail.com>
2025-03-04 10:35:54 +0200

    create add.php file

---
+++ departments/add.php
@@ -0,0 +1,14 @@
+<?php
+// UI
+
+require_once '../shared/header.php';
+require_once '../shared/navbar.php';
+
+?>
+
+<div class="container col-6 pt-5">
+    <h1 class="text-center">Add New Department</h1>
+    <div class="card-body bg-dark text-light">
+        <form method="POST"></form>
+    </div>
+</div>
